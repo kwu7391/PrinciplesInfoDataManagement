ab1b4e86b340d70430cc5edd9e94995bd0a4f330
kwu7391 <kylewu7391@gmail.com>
2024-12-10 18:25:57 -0500

    Update Mortgage.java

--- Project 2/demo/src/main/java/edu/cs213/mortgage/Mortgage.java
+++ Project 2/demo/src/main/java/edu/cs213/mortgage/Mortgage.java
@@ -1,7 +1,7 @@
 package edu.cs213.mortgage;
 
 public class Mortgage {
-    //private int applicationId;
+    private int applicationId;
     private String respondentId;
     private int loanType;
     private int loanAmount;
@@ -9,7 +9,7 @@ public class Mortgage {
     private int actionTaken;
     //private String msamdName;
     private int msamd;
-    private int countyCode;
+    //private int countyCode;
     private int applicantIncome;
     private double rateSpread;      
     private int purchaserType;
@@ -17,11 +17,11 @@ public class Mortgage {
     private int propertyType;
     private int loanPurpose;
 
-    public Mortgage(String respondentId, int loanType, int loanAmount,
+    public Mortgage(int applicationId, String respondentId, int loanType, int loanAmount,
                     int actionTaken, int msamd,
-                    int countyCode, int applicantIncome, double rateSpread, int purchaserType, int lienStatus, 
+                    int applicantIncome, double rateSpread, int purchaserType, int lienStatus, 
                     int propertyType, int loanPurpose) {
-        //this.applicationId = applicationId;
+        this.applicationId = applicationId;
         this.respondentId = respondentId;
         this.loanType = loanType;
         this.loanAmount = loanAmount;
@@ -29,7 +29,7 @@ public class Mortgage {
         this.actionTaken = actionTaken;
         //this.msamdName = msamdName;
         this.msamd = msamd;
-        this.countyCode = countyCode;
+        //this.countyCode = countyCode;
         this.applicantIncome = applicantIncome;
         this.rateSpread = rateSpread;
         this.purchaserType = purchaserType;
@@ -44,7 +44,7 @@ public class Mortgage {
     }
 
     // Other existing getters (if needed)
-    //public int getApplicationId() { return applicationId; }
+    public int getApplicationId() { return applicationId; }
     public String getRespondentId() { return respondentId; }
     public int getLoanType() { return loanType; }
     public int getLoanAmount() { return loanAmount; }
@@ -52,7 +52,7 @@ public class Mortgage {
     public int getActionTaken() { return actionTaken; }
     //public String getMsamdName() { return msamdName; }
     public int getMsamd() { return msamd; }
-    public int getCountyCode() { return countyCode; }
+    //public int getCountyCode() { return countyCode; }
     public int getApplicantIncome() { return applicantIncome; }
     public double getRateSpread() { return rateSpread; }
     public int getPurchaserType() { return purchaserType; }
