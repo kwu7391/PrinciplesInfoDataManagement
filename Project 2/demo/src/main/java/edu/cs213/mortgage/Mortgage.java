a21dd979c2d3e80bb2fe4a5db425a12f06c00c81
kwu7391 <kylewu7391@gmail.com>
2024-12-11 01:56:10 -0500

    Update Mortgage.java

--- Project 2/demo/src/main/java/edu/cs213/mortgage/Mortgage.java
+++ Project 2/demo/src/main/java/edu/cs213/mortgage/Mortgage.java
@@ -5,37 +5,33 @@ public class Mortgage {
     private String respondentId;
     private int loanType;
     private int loanAmount;
-    //private String actionTakenName;
     private int actionTaken;
-    //private String msamdName;
     private int msamd;
-    //private int countyCode;
     private int applicantIncome;
     private double rateSpread;      
     private int purchaserType;
     private int lienStatus;
     private int propertyType;
     private int loanPurpose;
+    private int ownerOccupancy;
 
     public Mortgage(int applicationId, String respondentId, int loanType, int loanAmount,
                     int actionTaken, int msamd,
                     int applicantIncome, double rateSpread, int purchaserType, int lienStatus, 
-                    int propertyType, int loanPurpose) {
+                    int propertyType, int loanPurpose, int ownerOccupancy) {
         this.applicationId = applicationId;
         this.respondentId = respondentId;
         this.loanType = loanType;
         this.loanAmount = loanAmount;
-        //this.actionTakenName = actionTakenName;
         this.actionTaken = actionTaken;
-        //this.msamdName = msamdName;
         this.msamd = msamd;
-        //this.countyCode = countyCode;
         this.applicantIncome = applicantIncome;
         this.rateSpread = rateSpread;
         this.purchaserType = purchaserType;
         this.lienStatus = lienStatus;
         this.propertyType = propertyType;
         this.loanPurpose = loanPurpose;
+        this.ownerOccupancy = ownerOccupancy;
     }
 
     // Getter for lienStatus
@@ -48,12 +44,10 @@ public class Mortgage {
     public String getRespondentId() { return respondentId; }
     public int getLoanType() { return loanType; }
     public int getLoanAmount() { return loanAmount; }
-    //public String getActionTakenName() { return actionTakenName; }
     public int getActionTaken() { return actionTaken; }
-    //public String getMsamdName() { return msamdName; }
     public int getMsamd() { return msamd; }
-    //public int getCountyCode() { return countyCode; }
     public int getApplicantIncome() { return applicantIncome; }
     public double getRateSpread() { return rateSpread; }
     public int getPurchaserType() { return purchaserType; }
+    public int getOwnerOccupancy() { return ownerOccupancy; }
 }
